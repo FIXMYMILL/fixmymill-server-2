eb8112d0c11b4606fb5458c3e27bb900aab79728
FIXMYMILL <148350886+FIXMYMILL@users.noreply.github.com>
2023-10-18 22:17:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const cors = require('cors');
+const app=express();
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+// app.use("/api/sheets",require("./routes/storeuser"));
+// app.use("/api/payments",require("./routes/createOrder"));
+// app.use("/api/payments",require("./routes/PaymentVerificarion"));
+// app.use("/api/payments",require("./routes/RequestKey"));
+
+
+var port = process.env.PORT || 4000;
+
+app.listen(port,async ()=>{
+    console.log("Server is on and running on port 4000");
+})
+
+
+
+
+
+
